22666251184a27285cb4590bfcfee7402ee86367
Sahil Shetty <70585867+ACENSONIX@users.noreply.github.com>
2022-06-21 04:00:15 +0530

    Added Nav,Footer,Property Component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 import { BaseUrl, fetchApi } from "../utils/fetchApi";
+import Property from "../components/Property";
 
 const Banner = ({
   purpose,
@@ -43,10 +44,8 @@ const Banner = ({
 );
 
 export default function Home({ propertyforrent, propertyforsale }) {
-  console.log(propertyforrent, propertyforsale);
   return (
-    <div>
-      <h1>HELLO </h1>
+    <Box>
       <Banner
         purpose="RENT A HOME"
         title1="Rental Homes for"
@@ -57,7 +56,11 @@ export default function Home({ propertyforrent, propertyforsale }) {
         ButtonText="Explore Renting"
         ImageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       />
-      <Flex flexWrap="wrap"></Flex>
+      <Flex flexWrap="wrap">
+        {propertyforrent.map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
+      </Flex>
       <Banner
         purpose="BUY A HOME"
         title1="Find,Buy & Own Your"
@@ -68,7 +71,12 @@ export default function Home({ propertyforrent, propertyforsale }) {
         ButtonText="Explore Buying"
         ImageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
-    </div>
+      <Flex flexWrap="wrap">
+        {propertyforsale.map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
+      </Flex>
+    </Box>
   );
 }
 
